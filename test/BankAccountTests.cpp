f1de7cb10822fe5be58ece1d7729f3da97e419de
lamemmett <lam.emmett@gmail.com>
2018-03-30 20:54:40 +0000

    Updated to use a test fixture for shared class object

--- test/BankAccountTests.cpp
+++ test/BankAccountTests.cpp
@@ -2,39 +2,41 @@
 #include "BankAccount.h"
 
 namespace {
-	TEST(BankAccountTests, InitialEmptyBalance)
-	{
-		BankAccount acc;
-		EXPECT_EQ(0, acc.GetBalance());
-	}
 
-	TEST(BankAccountTests, NegativeDeposit)
-	{
-		BankAccount acc;
-		EXPECT_THROW(acc.Deposit(-1), std::invalid_argument);
-	}
+class BankAccountTests : public ::testing::Test {
+protected:
+	BankAccount acc;
+};
 
-	TEST(BankAccountTests, BalanceAfterDeposit)
-	{
-		BankAccount acc;
-		acc.Deposit(10);
-		EXPECT_EQ(10, acc.GetBalance());
-	}
-
-	TEST(BankAccountTests, NegativeWithdrawal)
-	{
-		BankAccount acc;
-		EXPECT_THROW(acc.Withdraw(-5), std::invalid_argument);
-	}
-
-	TEST(BankAccountTests, InsufficientBalance)
-	{
-		BankAccount acc;
-		EXPECT_THROW(acc.Withdraw(5), std::out_of_range);
-
-		acc.Deposit(20);
-		EXPECT_THROW(acc.Withdraw(21), std::out_of_range);
-
-		EXPECT_NO_THROW(acc.Withdraw(20));
-	}
+TEST_F(BankAccountTests, InitialEmptyBalance)
+{
+	EXPECT_EQ(0, acc.GetBalance());
 }
+
+TEST_F(BankAccountTests, NegativeDeposit)
+{
+	EXPECT_THROW(acc.Deposit(-1), std::invalid_argument);
+}
+
+TEST_F(BankAccountTests, BalanceAfterDeposit)
+{
+	acc.Deposit(10);
+	EXPECT_EQ(10, acc.GetBalance());
+}
+
+TEST_F(BankAccountTests, NegativeWithdrawal)
+{
+	EXPECT_THROW(acc.Withdraw(-5), std::invalid_argument);
+}
+
+TEST_F(BankAccountTests, InsufficientBalance)
+{
+	EXPECT_THROW(acc.Withdraw(5), std::out_of_range);
+
+	acc.Deposit(20);
+	EXPECT_THROW(acc.Withdraw(21), std::out_of_range);
+
+	EXPECT_NO_THROW(acc.Withdraw(20));
+}
+
+} // namespace
